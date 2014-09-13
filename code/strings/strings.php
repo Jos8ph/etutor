5a97f5e5e42d893086e37512534bb30c9d6df2f4
dai.dquang@gmail.com <dai.dquang@gmail.com@082ba5ed-8914-83d3-ae7f-eec0f2ef1707>
2014-09-13 12:30:44 +0000

    - Add more image assets. - Page header is nearly completed, except navigation bar.

--- code/strings/strings.php
+++ code/strings/strings.php
@@ -5,5 +5,15 @@
  * Date: 9/12/14
  * Time: 10:19 PM
  */
-require 'strings_en.php';
-require 'strings_vi.php';
+displayTextDueToLanguage();
+
+function displayTextDueToLanguage() {
+    $lang = 'en';
+    switch ($lang) {
+        case 'en':
+            require 'strings_en.php';
+            break;
+        default:
+            require 'strings_vi.php';
+    }
+}
--- code/strings/strings_vi.php
+++ code/strings/strings_vi.php
@@ -6,3 +6,4 @@
  * Time: 10:03 PM
  */
 define('homepage_title', 'Trang chủ của Trường Học Trực Tuyến');
+define('login_signup', 'Đăng nhập | Đắng ký');
